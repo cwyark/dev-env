e2b7acedc30156e4b93268606f45c4e7bbbe4cb7
YuSheng <chester@cwyark.me>
2026-07-26 20:31:19 +0800

    improve neovim clipboard transport

--- config/nvim/lua/config/clipboard.lua
+++ config/nvim/lua/config/clipboard.lua
@@ -1,4 +1,11 @@
-local remote_session = vim.env.SSH_CONNECTION or vim.env.SSH_TTY or vim.env.ZELLIJ
+local remote_session = vim.env.SSH_CONNECTION ~= nil and vim.env.SSH_CONNECTION ~= ''
+  or vim.env.SSH_TTY ~= nil and vim.env.SSH_TTY ~= ''
+
+vim.api.nvim_create_user_command('ClipboardInfo', function()
+  local provider = vim.g.clipboard and vim.g.clipboard.name or 'auto-detected'
+  local location = remote_session and 'remote (OSC 52)' or 'local (Neovim provider)'
+  vim.notify(('Clipboard: %s\nProvider: %s'):format(location, provider))
+end, { desc = 'Show the active clipboard transport' })
 
 if not remote_session then
   return
